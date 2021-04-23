dcecaba9b85e15a4587bbc338687e6b214f7afee
Katherine Morgan <katherinemorgan8@outlook.com>
2021-04-23 16:18:19 -0700

    Practice DOM Manipulation

--- ts/main.ts
+++ ts/main.ts
@@ -4,6 +4,17 @@ window.onload = function() {
 }
 
 function main():void{
+    let msgHeading = document.createElement("h2");
+    msgHeading.innerText = "Processing form";
+    msgHeading.setAttribute("class","message");
+
+    let h1 = document.querySelector("h1");
+    h1.insertAdjacentElement("afterend", msgHeading);
+
+    setTimeout(function(){
+        msgHeading.remove();
+    }, 5000)
+
     resetErrorMessages();
     isTextPresent("first-name", "First name is required.");
     isTextPresent("last-name", "Last name is required.");
@@ -14,7 +25,6 @@ function main():void{
     if (!isDateValid(dob)){
         dobBox.nextElementSibling.innerHTML = "Format should be mm/dd/yyyy."
     }
-    
 }
 
 function isDateValid(input:string):boolean{
